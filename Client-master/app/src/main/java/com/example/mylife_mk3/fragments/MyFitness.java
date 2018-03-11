fde35902a96dd5cd7189fdc1bd94bf7cdb579165
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-03-11 22:27:44 +0200

    fitness adding to frontend

--- Client-master/app/src/main/java/com/example/mylife_mk3/fragments/MyFitness.java
+++ Client-master/app/src/main/java/com/example/mylife_mk3/fragments/MyFitness.java
@@ -1,25 +1,143 @@
 package com.example.mylife_mk3.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
 
 import com.example.mylife_mk3.R;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.widget.Toast;
+
+import okhttp3.*;
+
+import static java.sql.DriverManager.println;
 
 /**
  * Created by sally on 3/6/2018.
  */
 
 public class MyFitness extends android.app.Fragment {
-
+    private static final MediaType CONTENT_TYPE = MediaType.parse("application/json; charset=utf-8");
+    String name="";
+    String weightString="";
+    String heightString="";
+    String gender="";
+    String birthday="";
+    Toast toast;
+    JSONObject body;
+    int daily_steps=0;
     public  MyFitness(){
 
     }
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fitness_fragement, container, false);
+        final View rootView = inflater.inflate(R.layout.fitness_fragement, container, false);
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .url("http://10.0.2.2:8080/response")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                // No Internet connection.
+                println("Failed");
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response){
+
+                int statusCode = response.code();
+
+                try {
+                    String responseBody = response.body().string();
+                    body = new JSONObject(responseBody);
+                    if (body.has("key")) {
+                        JSONObject user = body.getJSONObject("key");
+                        Log.d("zozo", user.toString());
+                        int weight = user.getInt("weight");
+                        int height = user.getInt("height");
+                        Log.d("zozo", weight + "");
+                        name = user.getString("fullName");
+                        weightString = weight + "";
+                        heightString = height + "";
+                        gender = user.getString("gender");
+                        birthday = user.getString("dateOfBirth");
+                        daily_steps=user.getInt("averageDailySteps");
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView fullName = (TextView) rootView.findViewById(R.id.fullName);
+                                fullName.setText(name);
+
+                                LinearLayout weightLayout = (LinearLayout) rootView.findViewById(R.id.weight_layout);
+                                TextView weigttText = (TextView) weightLayout.findViewById(R.id.weight);
+                                weigttText.setText(weightString);
+
+                                TextView heightText = (TextView) rootView.findViewById(R.id.height);
+                                heightText.setText(heightString);
+
+                                TextView genderText = (TextView) rootView.findViewById(R.id.gender);
+                                genderText.setText(gender);
+
+                                TextView birthdatText = (TextView) rootView.findViewById(R.id.user_birthday_text);
+                                birthdatText.setText(birthday);
+
+                                TextView daily_stepsText = (TextView) rootView.findViewById(R.id.steps);
+                                daily_stepsText.setText(daily_steps+"");
+
+
+                            }
+                        });
+
+                    }
+                } catch (Exception e) {
+                    Log.d("exception",e.getMessage());
+                }
+                getActivity().runOnUiThread(new Runnable() {
+
+                    public void run() {
+                        if (!(body.has("key"))) {
+
+                            toast = Toast.makeText(getActivity().getApplicationContext(), "Please Login to your fitness", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.BOTTOM, 0, 70);
+                            toast.show();
+                            Log.d("zz", "d5l hna");
+
+                        }
+                    }
+                });
+
+
+            }
+        });
 
         return rootView;
     }
--- Client-master/app/src/main/java/com/example/mylife_mk3/fragments/MyStatsFragment.java
+++ Client-master/app/src/main/java/com/example/mylife_mk3/fragments/MyStatsFragment.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mylife_mk3.R;
 import com.example.mylife_mk3.activities.MyLocationsActivity;
@@ -17,9 +19,18 @@ import com.example.mylife_mk3.activities.MyProductivityActivity;
 import com.example.mylife_mk3.activities.MySleepActivity;
 import com.example.mylife_mk3.activities.MyTasksActivity;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Map;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+import static java.sql.DriverManager.println;
 
 
 public class MyStatsFragment extends android.app.Fragment {
@@ -34,7 +45,8 @@ public class MyStatsFragment extends android.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.stats_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.stats_fragment, container, false);
+        final View fitnessView  = inflater.inflate(R.layout.fitness_fragement,container,false);
 //        Button button = (Button) rootView.findViewById(R.id.mySleep);
 //        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/AlexandriaFLF.ttf");
 //        button.setTypeface(font);
@@ -112,6 +124,48 @@ public class MyStatsFragment extends android.app.Fragment {
                 Uri uri = Uri.parse("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CMZV&redirect_uri=http%3A%2F%2F10.0.2.2%3A8080&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
+
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .get()
+                        .url("http://10.0.2.2:8080/response")
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                        // No Internet connection.
+                        println("Failed");
+                        //call.cancel();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response){
+
+                        int statusCode = response.code();
+
+                        try{
+                            String responseBody = response.body().string();
+                            JSONObject body = new JSONObject(responseBody);
+                            //Log.d("zozo",responseBody);
+                            JSONObject user = body.getJSONObject("key");
+                            Log.d("zozo",user.toString());
+                            int weight = user.getInt("weight");
+                            int height = user.getInt("height");
+                            String name = user.getString("fullName");
+                            TextView fullName = (TextView) fitnessView.findViewById(R.id.fullName);
+                            fullName.setText(name);
+
+                            String weightS = weight+"";
+                            TextView weigttText = (TextView) rootView.findViewById(R.id.weight);
+                            weigttText.setText(weightS);
+
+
+                        } catch (Exception e) {
+                            println("Failed");
+                        }
+                    }
+                });
             }
         });
 
